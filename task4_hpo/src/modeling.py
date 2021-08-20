8816e4cdbb1bc4d939c03de2bbcb93a70f9c9daf
Siddharth Bhargava <sddhrth.bhargava@gmail.com>
2021-08-20 10:26:23 +0200

    combined

---
+++ task4_hpo/src/modeling.py
@@ -0,0 +1,199 @@
+# Local Imports
+from utils import BertBatchInput, BertSingleDatasetOutput, BertBatchOutput
+# Standard Imports
+
+# Third Party Imports
+import torch
+from torch import nn
+from torch.nn import MSELoss
+from transformers import BertPreTrainedModel, BertModel
+
+
+class ArgStrModel(BertPreTrainedModel):
+    """Handles weights and regressor initialization. Adjusted forward pass to allow for multi task learning."""
+
+    def __init__(self, config=None, dropout_prob=0.2, bert_hidden_layers=None,
+                 mlp_config=None, task_dict=None, device=None):
+        super().__init__(config)
+
+        self.bert = BertModel(config)
+        self.hidden_size = config.hidden_size
+        self.dropout_prob = dropout_prob
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
+        self.bert_hidden_layers = bert_hidden_layers
+        self.mlp_config = mlp_config
+        self.task_name = config.finetuning_task
+        if "MTLAS" in config.finetuning_task:
+            self.regressors = nn.ModuleDict()
+            for dataset in task_dict:
+                self.regressors[dataset] = self.generate_regressors(device)
+        else:
+            self.regressor = self.generate_regressors(device)
+
+    def generate_regressors(self, device):
+        """
+
+        :param device:
+        :return:
+        """
+
+        last_dim = self.hidden_size * self.bert_hidden_layers
+        layers = []
+        if self.mlp_config == 1:
+            layers.append(nn.Linear(in_features=last_dim, out_features=512, bias=False))
+            # layers.append(nn.BatchNorm1d(num_features=512))
+            layers.append(nn.ReLU())
+            if self.dropout_prob is not None:
+                layers.append(nn.Dropout(p=self.dropout_prob))
+            last_dim = 512
+        elif self.mlp_config == 2:
+            layers.append(nn.Linear(in_features=last_dim, out_features=100, bias=False))
+            # layers.append(nn.BatchNorm1d(num_features=100))
+            layers.append(nn.ReLU())
+            if self.dropout_prob is not None:
+                layers.append(nn.Dropout(p=self.dropout_prob))
+            last_dim = 100
+        elif self.mlp_config == 3:
+            layers.append(nn.Linear(in_features=last_dim, out_features=512, bias=False))
+            # layers.append(nn.BatchNorm1d(num_features=512))
+            layers.append(nn.ReLU())
+            if self.dropout_prob is not None:
+                layers.append(nn.Dropout(p=self.dropout_prob))
+            layers.append(nn.Linear(in_features=512, out_features=100, bias=False))
+            # layers.append(nn.BatchNorm1d(num_features=100))
+            layers.append(nn.ReLU())
+            if self.dropout_prob is not None:
+                layers.append(nn.Dropout(p=self.dropout_prob))
+            last_dim = 100
+        else:
+            layers.append(nn.Linear(in_features=last_dim, out_features=512, bias=False))
+            # layers.append(nn.BatchNorm1d(num_features=512))
+            layers.append(nn.ReLU())
+            if self.dropout_prob is not None:
+                layers.append(nn.Dropout(p=self.dropout_prob))
+            layers.append(nn.Linear(in_features=512, out_features=256, bias=False))
+            # layers.append(nn.BatchNorm1d(num_features=256))
+            layers.append(nn.ReLU())
+            if self.dropout_prob is not None:
+                layers.append(nn.Dropout(p=self.dropout_prob))
+            last_dim = 256
+        layers.append(nn.Linear(in_features=last_dim, out_features=1, bias=True))
+        layers.append(nn.Sigmoid())
+
+        return nn.Sequential(*layers).to(device)
+
+    def get_or_create_regressor(self, dataset, device):
+        """Creates or returns already created regressor for a task.
+
+        :param dataset:
+            Dataset being processed: gretz, toledo, swanson, UKPRank.
+        :param device:
+            Device (CPU or GPU) that will be used for training
+        :return: Regressor that will be used for the forward pass.
+        """
+        if self.regressors[dataset] is not None:
+            return self.regressors[dataset]
+        else:
+            self.regressors[dataset] = self.generate_regressors(device)
+            return self.regressors[dataset]
+
+    def forward(
+            self,
+            bert_batch_input: BertBatchInput,
+            calculate_loss: bool = True
+    ):
+        """
+        Performs a forward pass. In particular, separates output logits for each dataset and uses a dataset-specific
+        regression head (multi-task learning). Outputs are separated and returned for each dataset.
+
+        :param bert_batch_input:
+            The input infos needed to perform a forward pass (dataset, features, ...)
+        :param calculate_loss:
+            To calculate the MSELoss between the logits and losses.
+        :returns:
+            BertBatchOutput
+        """
+        single_dataset_outputs = []
+
+        for bert_batch_input in bert_batch_input.bert_single_dataset_inputs:
+            labels = bert_batch_input.labels
+            data_set = bert_batch_input.data_set
+            outputs = self.bert(
+                bert_batch_input.input_ids,
+                attention_mask=bert_batch_input.attention_mask,
+            )
+
+            hidden_states = outputs[-1]
+            out = torch.cat(tuple([hidden_states[-i] for i in range(1, self.bert_hidden_layers + 1)]), dim=-1)
+
+            # Pooling by also setting masked items to zero
+            bert_mask = bert_batch_input.attention_mask.unsqueeze(2)
+            # Multiply output with mask to only retain non-padding tokens
+            out = torch.mul(out, bert_mask)
+
+            # First item ['CLS'] is sentence representation
+            out = out[:, 0, :]
+
+            # Get the task-specific classifier
+            if "MTLAS" in self.task_name:
+                regressor = self.get_or_create_regressor(data_set, out.device)
+            else:
+                regressor = self.regressor
+            # Get the logits
+            logits = regressor(out)
+
+            if calculate_loss:
+                # We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss = None
+            single_dataset_outputs.append(BertSingleDatasetOutput(labels=labels, logits=logits,
+                                                                  loss=loss, data_set=data_set))
+
+        return BertBatchOutput(single_dataset_outputs)
+
+    def forward_for_inference(
+            self,
+            bert_batch_input: BertBatchInput,
+    ):
+        """
+
+        :param bert_batch_input:
+            BertBatchInput containing input_ids, attention_mask and labels info.
+        :return:
+           modified BertBatchOutput
+        """
+
+        per_batch_output = []
+        for bert_batch_input in bert_batch_input.bert_single_dataset_inputs:
+            labels = bert_batch_input.labels
+            dataset = bert_batch_input.data_set
+            outputs = self.bert(
+                bert_batch_input.input_ids,
+                attention_mask=bert_batch_input.attention_mask
+            )
+
+            hidden_states = outputs[-1]
+            out = torch.cat(tuple([hidden_states[-i] for i in range(1, self.bert_hidden_layers + 1)]), dim=-1)
+
+            # Pooling by also setting masked items to zero
+            bert_mask = bert_batch_input.attention_mask.unsqueeze(2)
+            # Multiply output with mask to only retain non-padding tokens
+            out = torch.mul(out, bert_mask)
+
+            # Extract First item ['CLS'] i.e. the sentence representation
+            out = out[:, 0, :]
+
+            logits = []
+            # run the out through each of the multi-regressor heads
+            for regressor in self.regressors:
+                regressor_unit = self.get_or_create_regressor(regressor, out.device)
+                # gets the logit value for this regressor and store in the list
+                logits.append(regressor_unit(out).to('cpu').numpy())
+
+            single_output = [labels, logits, dataset]
+            per_batch_output.append(single_output)
+
+        return per_batch_output
---
+++ task4_hpo/src/processors.py
@@ -0,0 +1,211 @@
+"""
+contains function that read input data files and generates features ~ batches
+
+"""
+# local Imports
+from utils import InputExample, InputFeatures, DataFile, Split, Columns, load_data_files, topic_splitter
+
+# Standard Imports
+from typing import List
+from abc import abstractmethod
+
+# Third Party Imports
+import numpy as np
+import mlflow
+from transformers import BertTokenizer
+
+
+class StrengthProcessor:
+    def __init__(self, data_files: List[DataFile], task_name: str, split_by_topic: bool = False):
+        """
+        Abstract processor with initialization for preprocessed data_files. Provides methods to create
+        examples for training/validation/testing. Processors differ by their task (labels, output_mode,...).
+
+        :param data_files:
+            data files for this processor
+        :param task_name:
+            name of the task to be performed
+        :param split_by_topic:
+            whether to re-distribute the train-dev-test sets by the dataset's topic distribution
+        """
+        self.task_name = task_name
+        self.data_files = data_files
+        if split_by_topic:
+            self.data_files = topic_splitter(self.data_files, seed=0)
+
+    def get_train_examples(self) -> List[InputExample]:
+        """See base class."""
+        return self._create_examples(self.data_files, "train", self.task_name)
+
+    def get_dev_examples(self) -> List[InputExample]:
+        """See base class."""
+        return self._create_examples(self.data_files, "dev", self.task_name)
+
+    def get_test_examples(self) -> List[InputExample]:
+        """See base class."""
+        return self._create_examples(self.data_files, "test", task_name=self.task_name)
+
+    @staticmethod
+    def _create_examples(data_files: List[DataFile],
+                         set_type: str = "train",
+                         task_name: str = None) -> List[InputExample]:
+        """
+        Creates examples for the training, dev and test sets.
+
+        :param data_files:
+            data files to extract the rows from
+        :param set_type:
+            the type of rows to extract (train/dev/test)
+
+        :returns:
+            examples for all matching rows in the data files.
+        """
+        examples = []
+        new_id = 0
+        for data_file in data_files:
+
+            if str("STLAS_LOO_" + data_file.dataset_name) in task_name and set_type != "test":
+                continue
+            if str("MTLAS_LOO_" + data_file.dataset_name) in task_name:
+                continue
+            if "only" in task_name and str("_" + data_file.dataset_name) not in task_name and set_type != "test":
+                continue
+
+            for (i, line) in enumerate(data_file.data_lines):
+                line_type = line[Columns.SET.value]
+                if i == 0 or not line_type == set_type:
+                    continue
+
+                guid = "%s-%s" % (set_type, new_id)
+                new_id += 1
+                text_a = line[Columns.SENTENCE.value]
+                # topic can be added here as text_b
+                if "source" in task_name:
+                    text_b = data_file.dataset_name
+                elif "topic" in task_name:
+                    text_b = line[Columns.TOPIC.value]
+                else:
+                    text_b = None
+                label = line[Columns.LABEL.value]
+                data_set = data_file.dataset_name
+                examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label,
+                                             data_set=data_set))
+        return examples
+
+    def get_task_name(self):
+        """Returns the name of the task the processor handles"""
+        return self.task_name
+
+
+# Combine lines of all data sets
+def get_combined_lines(processor) -> List:
+    """
+    Method to combine all dataset lines of a processor
+    :returns: list of all the lines from all the data sets
+    """
+    combined_lines = []
+    for data_file in processor.data_files:
+        combined_lines += (data_file.data_lines[1:])
+    return combined_lines
+
+
+def _convert_examples_to_features(
+        examples: List[InputExample],
+        tokenizer,
+        max_length: int,
+) -> List[InputFeatures]:
+    """
+    Converts dataset examples to InputFeatures
+
+    :param examples:
+        List of InputExamples to transform.
+    :param tokenizer:
+        The tokenizer to tokenize sentences + topics.
+    :param max_length:
+        The max token length.
+    :returns:
+        Tokenized Input Features from passed data examples.
+    """
+    if max_length is None:
+        max_length = tokenizer.max_len
+
+    labels = [float(example.label) if example.label is not None else None for example in examples]
+    data_sets = [example.data_set for example in examples]
+
+    # The actual encoding of sentences, with padding and max length! Text_a = sentences, text_b = Topics~ dataset_name!
+    batch_encoding = tokenizer.batch_encode_plus(
+        [(example.text_a, example.text_b) for example in examples], max_length=max_length, pad_to_max_length=True,
+    )
+
+    features = []
+    for i in range(len(examples)):
+        inputs = {k: batch_encoding[k][i] for k in batch_encoding}
+        feature = InputFeatures(**inputs, label=labels[i], data_set=data_sets[i])
+        features.append(feature)
+
+    return features
+
+
+def convert_features_to_dataset(processor, tokenizer, mode, max_token_length=None, max_token_length_perc=0.95):
+    """
+    Create the datasets
+    :return: train/dev/test dataset
+    """
+    if max_token_length is None:
+        max_token_length = find_max_length(tokenizer, processor, p=max_token_length_perc)
+        mlflow.log_param("max token length", max_token_length)
+
+    if mode == "dev":
+        features = _convert_examples_to_features(processor.get_dev_examples(), tokenizer,
+                                                 max_token_length)
+    elif mode == "test":
+        features = _convert_examples_to_features(processor.get_test_examples(), tokenizer,
+                                                 max_token_length)
+    else:
+        features = _convert_examples_to_features(processor.get_train_examples(), tokenizer,
+                                                 max_token_length)
+
+    return features
+
+
+def find_max_length(tokenizer, processor, p: float) -> int:
+    """
+    :tokenizer: BERT tokenizer used for tokenizing the data
+    :processor: The Processor containing the data files
+    :p: percentile used to decide the maximum token length to be used.
+
+    :return: maximum token length to be used
+    """
+    combined_lines = get_combined_lines(processor)
+    sens = [(line[Columns.SENTENCE.value]) for line in combined_lines]
+    batch_encoding = tokenizer.batch_encode_plus(
+        sens, pad_to_max_length=False,
+    )
+    # Collect all lengths
+    lens = [len(x) for x in batch_encoding['input_ids']]
+    # Get the p percentile of lengths.
+    res = int(np.percentile(lens, p * 100))
+
+    return res
+
+
+def get_datasets(config):
+    data_files, task_dict = load_data_files(config["data_dir"], config["task_name"])
+    # initialize the Processor
+    processor = StrengthProcessor(data_files, task_name=config["task_name"])
+    if "cased" in config["bert_arch"]:
+        tokenizer = BertTokenizer.from_pretrained(config["bert_arch"], do_lower_case=True)
+    else:
+        tokenizer = BertTokenizer.from_pretrained(config["bert_arch"], do_lower_case=False)
+
+    # prepare the train and validation datasets
+    train_dataset = convert_features_to_dataset(processor, tokenizer,
+                                                "train", config["max_seq_length"], config["max_seq_length_perc"])
+    validation_dataset = convert_features_to_dataset(processor, tokenizer,
+                                                     "dev", config["max_seq_length"], config["max_seq_length_perc"])
+    test_dataset = convert_features_to_dataset(processor, tokenizer, "test",
+                                               config["max_seq_length"], config["max_seq_length_perc"])
+
+    print("Length of training:", len(train_dataset))
+    print("Length of testing:", len(test_dataset))
+    return train_dataset, validation_dataset, test_dataset, task_dict
